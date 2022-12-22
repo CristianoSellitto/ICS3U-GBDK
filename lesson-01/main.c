6d41b4d04f9f6c71d0828bcf903a2b62284f2dab
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-12-22 16:10:57 +0000

    first commit

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// Copyright Cristiano 2022
+//
+// Created by Cristiano Sellitto
+// Created in December 2022
+// A file for the "Space Aliens" game
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!");
+}
